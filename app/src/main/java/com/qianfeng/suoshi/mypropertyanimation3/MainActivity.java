70ea4df074c942353f3b1ce170c4a0532dc2c9c7
wiatt <wiatt0216@163.com>
2019-02-17 09:57:46 +0800

    git上传，第三次尝试

--- app/src/main/java/com/qianfeng/suoshi/mypropertyanimation3/MainActivity.java
+++ app/src/main/java/com/qianfeng/suoshi/mypropertyanimation3/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        test();
+        test(true);
     }
 
     public void click(View view){
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public boolean test(){
+    public boolean test(boolean b){
         return true;
     }
 }
